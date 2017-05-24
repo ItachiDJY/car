9f57ad15c1e43ae51414e85759799b49fb795786
JY <857756418@qq.com>
2017-05-24 15:28:56 +0800

    后台模板布局文件

---
+++ laravel/resources/views/layouts/main.blade.php
@@ -0,0 +1,210 @@
+<ul class="nav nav-list">
+						<li class="active">
+							<a href="index">
+								<i class="icon-dashboard"></i>
+								<span class="menu-text"> 后台控制台 </span>
+							</a>
+						</li>
+
+						<li>
+							<a href="#" class="dropdown-toggle">
+								<i class="icon-desktop"></i>
+								<span class="menu-text"> 员工管理 </span>
+
+								<b class="arrow icon-angle-down"></b>
+							</a>
+
+							<ul class="submenu">
+								<li>
+									<a href="release.html">
+										<i class="icon-double-angle-right"></i>
+										添加员工
+									</a>
+								</li>
+
+								<li>
+									<a href="record.html">
+										<i class="icon-double-angle-right"></i>
+										员工离职
+									</a>
+								</li>
+
+								<li>
+									<a href="#" class="dropdown-toggle">
+										<i class="icon-double-angle-right"></i>
+											司机管理
+										
+										<b class="arrow icon-angle-down"></b>
+									</a>
+
+									<ul class="submenu">
+										<li>
+											<a href="account.html">
+												<i class="icon-leaf"></i>
+												   司机列表
+											</a>
+										</li>
+
+										
+
+									</ul>
+								</li>
+							</ul>
+						</li>
+
+						<li>
+							<a href="#" class="dropdown-toggle">
+								<i class="icon-list"></i>
+								<span class="menu-text"> 车辆管理 </span>
+
+								<b class="arrow icon-angle-down"></b>
+							</a>
+
+							<ul class="submenu">
+								<li>
+									<a href="car_add">
+										<i class="icon-double-angle-right"></i>
+										添加车辆信息
+									</a>
+								</li>
+
+								<li>
+									<a href="car">
+										<i class="icon-double-angle-right"></i>
+										车辆信息展示
+									</a>
+								</li>
+								<li>
+									<a href="car">
+										<i class="icon-double-angle-right"></i>
+										添加配置
+									</a>
+								</li>
+								<li>
+									<a href="car">
+										<i class="icon-double-angle-right"></i>
+										车辆配置展示
+									</a>
+								</li>
+                                
+                                
+							</ul>
+						</li>
+
+						<li>
+							<a href="#" class="dropdown-toggle">
+								<i class="icon-edit"></i>
+								<span class="menu-text"> 会员管理 </span>
+
+								<b class="arrow icon-angle-down"></b>
+							</a>
+
+							<ul class="submenu">
+								<li>
+									<a href="form-elements.html">
+										<i class="icon-double-angle-right"></i>
+										添加会员
+									</a>
+								</li>
+
+								<li>
+									<a href="form-wizard.html">
+										<i class="icon-double-angle-right"></i>
+										删除会员
+									</a>
+								</li>
+								<li>
+									<a href="form-wizard.html">
+										<i class="icon-double-angle-right"></i>
+										会员列表
+									</a>
+								</li>
+								<li>
+									<a href="form-wizard.html">
+										<i class="icon-double-angle-right"></i>
+										会员级别列表
+									</a>
+								</li>
+
+							</ul>
+						</li>
+						<li>
+							<a href="#" class="dropdown-toggle">
+								<i class="icon-edit"></i>
+								<span class="menu-text"> 订单管理 </span>
+
+								<b class="arrow icon-angle-down"></b>
+							</a>
+
+							<ul class="submenu">
+								<li>
+									<a href="form-elements.html">
+										<i class="icon-double-angle-right"></i>
+										订单列表
+									</a>
+								</li>
+
+								<li>
+									<a href="form-wizard.html">
+										<i class="icon-double-angle-right"></i>
+										回收站
+									</a>
+								</li>
+								
+
+							</ul>
+						</li>
+						<li>
+							<a href="#" class="dropdown-toggle">
+								<i class="icon-edit"></i>
+								<span class="menu-text"> 支付管理 </span>
+
+								<b class="arrow icon-angle-down"></b>
+							</a>
+
+							<ul class="submenu">
+								<li>
+									<a href="form-elements.html">
+										<i class="icon-double-angle-right"></i>
+										支付宝支付
+									</a>
+								</li>
+
+								<li>
+									<a href="form-wizard.html">
+										<i class="icon-double-angle-right"></i>
+										微信支付
+									</a>
+								</li>
+								
+
+							</ul>
+						</li>
+						<li>
+							<a href="#" class="dropdown-toggle">
+								<i class="icon-edit"></i>
+								<span class="menu-text"> 门店管理 </span>
+
+								<b class="arrow icon-angle-down"></b>
+							</a>
+
+							<ul class="submenu">
+								<li>
+									<a href="form-elements.html">
+										<i class="icon-double-angle-right"></i>
+										门店列表
+									</a>
+								</li>
+
+								<li>
+									<a href="form-wizard.html">
+										<i class="icon-double-angle-right"></i>
+										添加新门店
+									</a>
+								</li>
+								
+
+							</ul>
+						</li>
+
+					</ul>
