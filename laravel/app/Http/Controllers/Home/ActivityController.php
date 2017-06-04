443dc4cd536198dbfc7cf08366aa1a9d37ee20bc
JY <857756418@qq.com>
2017-06-04 19:35:38 +0800

    前台活动展示

--- laravel/app/Http/Controllers/Home/ActivityController.php
+++ laravel/app/Http/Controllers/Home/ActivityController.php
@@ -5,12 +5,30 @@ namespace App\Http\Controllers\Home;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Models\Active;
 use App\Http\Controllers\Controller;
 
 class ActivityController extends Controller
 {
     public function index()
     {
-        return view('Home/Activity/activity');
+    	$time = time();
+    	$actObj = new Active;
+    	$data = $actObj->selectAll();
+    	foreach ($data as $key => $val) {
+    		if($val['end_time'] < $time) {
+    			$data[$key]['status'] = '已结束';
+    			$data[$key]['css'] = 'closed';
+    		} elseif ($val['end_time'] - $time < 3600*24) {
+    			$data[$key]['status'] = '即将结束';
+    			$data[$key]['css'] = 'coming-close';
+    		} else {
+    			$data[$key]['status'] = '进行中';
+    			$data[$key]['css'] = 'progress';
+    		}
+    	}
+    	
+    	//print_r($data);die;
+        return view('Home/Activity/activity',['data' => $data]);
     }
 }
