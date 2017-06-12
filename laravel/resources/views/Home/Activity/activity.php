cff0fec06b39403566025aecef12ae5440dfaa97
JY <857756418@qq.com>
2017-06-12 17:16:50 +0800

    0612

--- laravel/resources/views/Home/Activity/activity.php
+++ laravel/resources/views/Home/Activity/activity.php
@@ -94,21 +94,15 @@
                 <li  >
                     <a href="/long" onClick="dcsMultiTrack('DCS.dcsuri', '/zuchepc.event','WT.mc_click','首页-长租（顶）')">长租</a>
                 </li>
-                <li  >
-                    <a href="/company" onClick="dcsMultiTrack('DCS.dcsuri', '/zuchepc.event','WT.mc_click','首页-企业租车（顶）')">企业租车</a>
-                </li>
-                <li  >
-                    <a href="/free" onClick="dcsMultiTrack('DCS.dcsuri', '/zuchepc.event','WT.mc_click','首页-顺风车（顶）')">顺风车</a>
-                </li>
+            
+                
                 <li  >
                     <a href="/store" onClick="dcsMultiTrack('DCS.dcsuri', '/zuchepc.event','WT.mc_click','首页-门店（顶）')">门店</a>
                 </li>
                 <li>
                     <a href="/activity" onClick="dcsMultiTrack('DCS.dcsuri', '/zuchepc.event','WT.mc_click','首页-活动（顶）')">活动</a>
                 </li>
-                <li  >
-                    <a href="/country" onClick="dcsMultiTrack('DCS.dcsuri', '/zuchepc.event','WT.mc_click','首页-国际租车（顶）')">国际租车</a>
-                </li>
+               
             </ul>
             <div class="zcindex_login" >
                 <div class="wdl" id="notLogin">
